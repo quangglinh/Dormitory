8daed07fbd7dd1e4996ed4e8df81644834d1fe4b
nguyenlam <lamnsse06118@fpt.edu.vn>
2018-11-03 00:15:01 +0700

    add DBUtility and Entity + Model folders

---
+++ DormintoryStudentApp/Supporting Files/DBUltiity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace DormintoryStudentApp.Supporting_Files
+{
+    class DBUltiity
+    {
+        class DbUtil
+        {
+            public static SqlConnection getConnection
+            {
+                get
+                {
+                    string connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+                    return new SqlConnection(connectionString);
+                }
+            }
+        }
+    }
+}
