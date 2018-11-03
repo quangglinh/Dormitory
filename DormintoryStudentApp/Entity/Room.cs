746c4a756ed81a85683e642bb7bf44747d3ca3d8
nguyenlam <lamnsse06118@fpt.edu.vn>
2018-11-03 07:14:20 +0700

    create some entities

---
+++ DormintoryStudentApp/Entity/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DormintoryStudentApp.Entity
+{
+    class Room
+    {
+        public string roomID { get; set; }
+        public string dom { get; set; }
+        public double monthyFee { get; set; }
+        public int numberOfSlot { get; set; }
+    }
+}
